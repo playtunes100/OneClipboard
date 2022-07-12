e0875487754bd4ca79fe75d92d3fc049009d47cc
playtunes <marcelmashaba@gmail.com>
2022-07-12 19:19:31 +0200

    Add project files.

---
+++ OneClipboard/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace OneClipboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Title = new System.Windows.Forms.Label();
+            this.richTextBoxClipboard = new System.Windows.Forms.RichTextBox();
+            this.labelRecentlyAdded = new System.Windows.Forms.Label();
+            this.checkBoxStartup = new System.Windows.Forms.CheckBox();
+            this.buttonAddtoClip = new System.Windows.Forms.Button();
+            this.labelFileSelector = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelFileSelected = new System.Windows.Forms.Label();
+            this.buttonStartWatch = new System.Windows.Forms.Button();
+            this.labelStartWatch = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            this.sharpClipboard = new WK.Libraries.SharpClipboardNS.SharpClipboard(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold);
+            this.Title.Location = new System.Drawing.Point(199, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(115, 23);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "OneClipboard";
+            // 
+            // richTextBoxClipboard
+            // 
+            this.richTextBoxClipboard.Location = new System.Drawing.Point(24, 60);
+            this.richTextBoxClipboard.Name = "richTextBoxClipboard";
+            this.richTextBoxClipboard.Size = new System.Drawing.Size(485, 173);
+            this.richTextBoxClipboard.TabIndex = 1;
+            this.richTextBoxClipboard.Text = "";
+            // 
+            // labelRecentlyAdded
+            // 
+            this.labelRecentlyAdded.AutoSize = true;
+            this.labelRecentlyAdded.Location = new System.Drawing.Point(21, 44);
+            this.labelRecentlyAdded.Name = "labelRecentlyAdded";
+            this.labelRecentlyAdded.Size = new System.Drawing.Size(83, 13);
+            this.labelRecentlyAdded.TabIndex = 2;
+            this.labelRecentlyAdded.Text = "Recently Added";
+            // 
+            // checkBoxStartup
+            // 
+            this.checkBoxStartup.AutoSize = true;
+            this.checkBoxStartup.Location = new System.Drawing.Point(24, 240);
+            this.checkBoxStartup.Name = "checkBoxStartup";
+            this.checkBoxStartup.Size = new System.Drawing.Size(95, 17);
+            this.checkBoxStartup.TabIndex = 3;
+            this.checkBoxStartup.Text = "Run at Startup";
+            this.checkBoxStartup.UseVisualStyleBackColor = true;
+            this.checkBoxStartup.CheckedChanged += new System.EventHandler(this.checkBoxStartup_CheckedChanged);
+            // 
+            // buttonAddtoClip
+            // 
+            this.buttonAddtoClip.Location = new System.Drawing.Point(401, 240);
+            this.buttonAddtoClip.Name = "buttonAddtoClip";
+            this.buttonAddtoClip.Size = new System.Drawing.Size(109, 23);
+            this.buttonAddtoClip.TabIndex = 4;
+            this.buttonAddtoClip.Text = "Sync Clipboard";
+            this.buttonAddtoClip.UseVisualStyleBackColor = true;
+            this.buttonAddtoClip.Click += new System.EventHandler(this.buttonAddtoClip_Click);
+            // 
+            // labelFileSelector
+            // 
+            this.labelFileSelector.AutoSize = true;
+            this.labelFileSelector.Location = new System.Drawing.Point(24, 273);
+            this.labelFileSelector.Name = "labelFileSelector";
+            this.labelFileSelector.Size = new System.Drawing.Size(247, 13);
+            this.labelFileSelector.TabIndex = 5;
+            this.labelFileSelector.Text = "Select or Create clipboard.txt file in OneDrive folder";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelFileSelected
+            // 
+            this.labelFileSelected.AutoSize = true;
+            this.labelFileSelected.Location = new System.Drawing.Point(105, 295);
+            this.labelFileSelected.MaximumSize = new System.Drawing.Size(230, 30);
+            this.labelFileSelected.Name = "labelFileSelected";
+            this.labelFileSelected.Size = new System.Drawing.Size(88, 13);
+            this.labelFileSelected.TabIndex = 7;
+            this.labelFileSelected.Text = "No File Selected.";
+            // 
+            // buttonStartWatch
+            // 
+            this.buttonStartWatch.Location = new System.Drawing.Point(339, 285);
+            this.buttonStartWatch.Name = "buttonStartWatch";
+            this.buttonStartWatch.Size = new System.Drawing.Size(51, 23);
+            this.buttonStartWatch.TabIndex = 8;
+            this.buttonStartWatch.Text = "Start";
+            this.buttonStartWatch.UseVisualStyleBackColor = true;
+            this.buttonStartWatch.Click += new System.EventHandler(this.buttonStartWatch_Click);
+            // 
+            // labelStartWatch
+            // 
+            this.labelStartWatch.AutoSize = true;
+            this.labelStartWatch.Location = new System.Drawing.Point(396, 290);
+            this.labelStartWatch.Name = "labelStartWatch";
+            this.labelStartWatch.Size = new System.Drawing.Size(108, 13);
+            this.labelStartWatch.TabIndex = 9;
+            this.labelStartWatch.Text = "Not Watching Folder.";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "OneCliboard";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // fileSystemWatcher
+            // 
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+            this.fileSystemWatcher.Filter = "*.txt*";
+            this.fileSystemWatcher.NotifyFilter = ((System.IO.NotifyFilters)((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.LastWrite)));
+            this.fileSystemWatcher.SynchronizingObject = this;
+            this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            // 
+            // sharpClipboard
+            // 
+            this.sharpClipboard.MonitorClipboard = true;
+            this.sharpClipboard.ObservableFormats.All = true;
+            this.sharpClipboard.ObservableFormats.Files = false;
+            this.sharpClipboard.ObservableFormats.Images = false;
+            this.sharpClipboard.ObservableFormats.Others = false;
+            this.sharpClipboard.ObservableFormats.Texts = true;
+            this.sharpClipboard.ObserveLastEntry = true;
+            this.sharpClipboard.Tag = null;
+            this.sharpClipboard.ClipboardChanged += new System.EventHandler<WK.Libraries.SharpClipboardNS.SharpClipboard.ClipboardChangedEventArgs>(this.sharpClipboard_ClipboardChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(531, 342);
+            this.Controls.Add(this.labelStartWatch);
+            this.Controls.Add(this.buttonStartWatch);
+            this.Controls.Add(this.labelFileSelected);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelFileSelector);
+            this.Controls.Add(this.buttonAddtoClip);
+            this.Controls.Add(this.checkBoxStartup);
+            this.Controls.Add(this.labelRecentlyAdded);
+            this.Controls.Add(this.richTextBoxClipboard);
+            this.Controls.Add(this.Title);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "OneClipboard";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.RichTextBox richTextBoxClipboard;
+        private System.Windows.Forms.Label labelRecentlyAdded;
+        private System.Windows.Forms.CheckBox checkBoxStartup;
+        private System.Windows.Forms.Button buttonAddtoClip;
+        private System.Windows.Forms.Label labelFileSelector;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelFileSelected;
+        private System.Windows.Forms.Button buttonStartWatch;
+        private System.Windows.Forms.Label labelStartWatch;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.IO.FileSystemWatcher fileSystemWatcher;
+        private WK.Libraries.SharpClipboardNS.SharpClipboard sharpClipboard;
+    }
+}
+
---
+++ OneClipboard/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using WK.Libraries.SharpClipboardNS;
+
+namespace OneClipboard
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // sets opacity to 100 if the app is not found in the startup registry
+            // or set to 0 if it is
+            if (rkApp.GetValue("OneClipboard") == null)
+            {
+                this.Opacity = 100;
+            }
+            else
+            {
+                checkBoxStartup.Checked = true;
+                this.Opacity = 0;
+            }
+            string clipboard_path;
+            try { clipboard_path = Properties.Settings.Default["clipboard_file_path"].ToString(); }
+            catch { clipboard_path = ""; }
+            if (string.IsNullOrEmpty(clipboard_path))
+            {
+                labelFileSelected.Text = "No File Selected.";
+            }
+            else
+            {
+                labelFileSelected.Text = Properties.Settings.Default["clipboard_file_path"].ToString();
+                fileSystemWatcher.Path = Path.GetDirectoryName(Properties.Settings.Default["clipboard_file_path"].ToString());
+                labelStartWatch.Text = "Watching folder...";
+            }
+            getClipboardData();
+        }
+        // opens file dialog to allow file selection for the text file
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Properties.Settings.Default["clipboard_file_path"] = openFileDialog.FileName;
+                Properties.Settings.Default.Save();
+                labelFileSelected.Text = Properties.Settings.Default["clipboard_file_path"].ToString();
+            }
+        }
+
+        // changes the window's opacity as the icon in the system tray is clicked
+        // change if a better way of making the window visible is known
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+            {
+                this.Opacity = 0;
+            }
+            else
+            {
+                this.Opacity = 100;
+                this.Activate();
+            }
+        }
+
+        // Sets the app to launch on startup if checkBox is set to Checked
+        private void checkBoxStartup_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxStartup.Checked)
+            {
+                rkApp.SetValue("OneClipboard", Application.ExecutablePath);
+            }
+            else
+            {
+                rkApp.DeleteValue("OneClipboard", false);
+            }
+        }
+
+        // adds text in the RichTextBox to the clipboard file
+        private void buttonAddtoClip_Click(object sender, EventArgs e)
+        {
+            //for now this button writes to the file which will then be read by file system watcher
+            writeClipboardFile(richTextBoxClipboard.Text);
+            //Clipboard.SetText(richTextBox1.Text);
+        }
+
+        // Start listening to the file changes for updates
+        private void buttonStartWatch_Click(object sender, EventArgs e)
+        {
+            string clipboard_path;
+            try { clipboard_path = Properties.Settings.Default["clipboard_file_path"].ToString(); }
+            catch { clipboard_path = ""; }
+            if (!string.IsNullOrEmpty(clipboard_path))
+            {
+
+                fileSystemWatcher.Path = Path.GetDirectoryName(Properties.Settings.Default["clipboard_file_path"].ToString());
+                labelStartWatch.Text = "Watching folder...";
+            }
+        }
+
+        private void fileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            readClipboardFile();
+        }
+
+
+        private void getClipboardData()
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text))
+            {
+                string clipData = (String)Clipboard.GetDataObject().GetData(DataFormats.Text);
+
+                richTextBoxClipboard.Text = clipData;
+            }
+        }
+
+        private void readClipboardFile()
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(Properties.Settings.Default["clipboard_file_path"].ToString()))
+                {
+                    // Read the stream as a string
+                    string line = sr.ReadToEnd();
+                    if (line != (String)Clipboard.GetDataObject().GetData(DataFormats.Text))
+                    {
+                        Clipboard.SetText(line);
+                        getClipboardData();
+                    }
+
+
+                }
+            }
+            catch (Exception exception)
+            {
+                // Let the user know what went wrong.
+                //MessageBox.Show("The file could not be read:\n Error Message: " + exception.Message);
+
+            }
+        }
+
+        //writes data to he clipboard file in OneDrive
+        private void writeClipboardFile(string data)
+        {
+            using (StreamWriter sw = new StreamWriter(Properties.Settings.Default["clipboard_file_path"].ToString()))
+            {
+                sw.Write(data);
+                
+
+            }
+
+        }
+
+        private void sharpClipboard_ClipboardChanged(object sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            getClipboardData();
+        }
+    }
+}
